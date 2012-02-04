4692b29fa0be43b07685127acc369c2986d6d426
vrighter <vrighter@vrighters-laptop>
2012-02-04 02:48:05 +0100

    Added some code in the ExpressionGenerator class in the tokenizer method that should fix a bug where if a function, sub, array name or variable name starts with a number, they would have been tokenized as a constant and a variable name. It now checks whether the next character after the constant is one of those allowed in a variable name, and if so detects an error.
    Also made the convertInfixToPostfix method return a String array instead of a space delimited string. and removed the code that removed all spaces from within a function call or an array access which would lead, for example, to the following function call within an expression a[b c] being parsed as a[bc].

--- brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/ExpressionGenerator.java
+++ brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/ExpressionGenerator.java
@@ -3,6 +3,7 @@ package brainfuckcompiler.compiler.expressions;
 import brainfuckcompiler.compiler.expressions.operators.Operator;
 import brainfuckcompiler.compiler.expressions.operators.Operators;
 import brainfuckcompiler.statics;
+import java.util.ArrayList;
 import java.util.Stack;
 import java.util.regex.Matcher;
 
@@ -13,23 +14,22 @@ import java.util.regex.Matcher;
 public class ExpressionGenerator
 {
 
-    private static String convertInfixToPostfix(String e)
+    private static String[] convertInfixToPostfix(String e)
     {
         Operator paren = new Operator("(", "\\(", Integer.MAX_VALUE, true), temp;
         Stack s = new Stack();
-        StringBuilder b = new StringBuilder();
+        ArrayList<String> retArrayList = new ArrayList<String>();
         String[] tokens = tokenize(e);
         if (tokens == null)
         {
-            return "";
+            return null;
         }
         for (int i = 0; i < tokens.length; i++)
         {
             String t = tokens[i];
             if (t.matches("([_a-zA-Z][_0-9a-zA-Z]*\\[.*\\])|([_a-zA-Z][_0-9a-zA-Z]*\\(.*\\))|([_a-zA-Z][_0-9a-zA-Z]*)|([0-9]+)"))
             {
-                b.append(t);
-                b.append(" ");
+                retArrayList.add(t);
             } else if (t.equals("("))
             {
                 s.push(paren);
@@ -37,8 +37,7 @@ public class ExpressionGenerator
             {
                 while (!(temp = (Operator) s.pop()).getOp().equals("("))
                 {
-                    b.append(temp.getOp());
-                    b.append(' ');
+                    retArrayList.add(temp.getOp());
                 }
             } else
             {
@@ -58,8 +57,7 @@ public class ExpressionGenerator
                         Operator topStack = (Operator) s.peek();
                         if ((topStack.isLeftAssociative() && topStack.getPrecedence() <= o.getPrecedence()) || (!topStack.isLeftAssociative() && topStack.getPrecedence() < o.getPrecedence()))
                         {
-                            b.append(((Operator) s.pop()).getOp());
-                            b.append(' ');
+                            retArrayList.add(((Operator) s.pop()).getOp());
                         } else
                         {
                             s.push(o);
@@ -71,18 +69,17 @@ public class ExpressionGenerator
         }
         while (!s.empty())
         {
-            b.append(((Operator) s.pop()).getOp());
-            b.append(" ");
+            retArrayList.add(((Operator) s.pop()).getOp());
         }
-        System.out.println(b.substring(0, b.length() - 1));
-        System.exit(0);
-        return b.substring(0, b.length() - 1);
+        String[] ret = new String[retArrayList.size()];
+        retArrayList.toArray(ret);
+        return ret;
     }
 
     private static String[] tokenize(String e)
     {
         Matcher m = statics.p.matcher(e);
-        java.util.ArrayList<String> l = new java.util.ArrayList<String>();
+        ArrayList<String> l = new ArrayList<String>();
         while (m.find())
         {
             String s = m.group();
@@ -101,15 +98,14 @@ public class ExpressionGenerator
                         counter++;
                     } else if (c == ')')
                     {
-                        counter--;
-                    }
-                    if (counter == 0)
-                    {
-                        l.add(e.substring(startingPosition, endingPosition).replace(" ", ""));
-                        e = e.substring(endingPosition);
-                        m.reset(e);
-                        found = true;
-                        break;
+                        if (--counter == 0)
+                        {
+                            l.add(e.substring(startingPosition, endingPosition));
+                            e = e.substring(endingPosition);
+                            m.reset(e);
+                            found = true;
+                            break;
+                        }
                     }
                 }
                 if (!found)
@@ -132,15 +128,14 @@ public class ExpressionGenerator
                         counter++;
                     } else if (c == ']')
                     {
-                        counter--;
-                    }
-                    if (counter == 0)
-                    {
-                        l.add(e.substring(startingPosition, endingPosition).replace(" ", ""));
-                        e = e.substring(endingPosition);
-                        m.reset(e);
-                        found = true;
-                        break;
+                        if (--counter == 0)
+                        {
+                            l.add(e.substring(startingPosition, endingPosition));
+                            e = e.substring(endingPosition);
+                            m.reset(e);
+                            found = true;
+                            break;
+                        }
                     }
                 }
                 if (!found)
@@ -148,9 +143,29 @@ public class ExpressionGenerator
                     System.out.println("No matching ] found");
                     System.exit(0);
                 }
+            } else if (s.matches("[0-9]+"))
+            {
+                int position = e.indexOf(s) + s.length();
+                if (position == e.length())
+                {
+                    l.add(s);
+                } else
+                {
+                    char c = e.charAt(position);
+                    if (c != '_' && (!((c >= 'a') && (c <= 'z'))) && (!((c >= 'A') && (c <= 'Z'))))
+                    {
+                        l.add(s);
+                        e = e.substring(position);
+                        m.reset(e);
+                    } else
+                    {
+                        System.out.println("Could not match token");
+                        System.exit(0);
+                    }
+                }
             } else
             {
-                l.add(m.group());
+                l.add(s);
             }
         }
         String[] ret = new String[l.size()];
@@ -168,9 +183,13 @@ public class ExpressionGenerator
         return generateCodeForPostfixExpression(convertInfixToPostfix(s));
     }
 
-    private static int generateCodeForPostfixExpression(String s)
+    private static int generateCodeForPostfixExpression(String[] tokens)
     {
-        String[] tokens = s.split(" ");
+        for (String s : tokens)
+        {
+            System.out.println(s);
+        }
+        System.exit(0);
         Node tree = Operators.createNode(tokens[tokens.length - 1]);
         tree.populate(tokens, tokens.length - 2);
         return tree.generateBF();
