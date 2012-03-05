39b92fa15af8d49553b6625055da7b6ab7e69031
vrighter <vrighter@vrighters-laptop>
2012-03-05 20:42:18 +0100

    started porting of code generation from brainfucktools to the actual operator nodes where possible... they make more sense there, and will allow for some optimizations better.
    Started optimization of code generated by EqualityNode

--- brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/nodes/EqualityOperator.java
+++ brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/nodes/EqualityOperator.java
@@ -1,6 +1,8 @@
 package brainfuckcompiler.compiler.expressions.nodes;
 
 import brainfuckcompiler.compiler.expressions.nodetypes.BinaryOperator;
+import brainfuckcompiler.compiler.expressions.nodetypes.ConstantNode;
+import brainfuckcompiler.compiler.expressions.nodetypes.VariableNode;
 import brainfuckcompiler.compiler.program.structure.Block;
 import brainfuckcompiler.statics;
 
@@ -11,31 +13,96 @@ import brainfuckcompiler.statics;
 public class EqualityOperator extends BinaryOperator
 {
 
-    /**
-     *
-     */
-    public EqualityOperator(int lineNumber, Block parentBlock)
-    {
-        super(lineNumber, parentBlock);
-        returnsBoolean = true;
-    }
+	/**
+	 *
+	 */
+	public EqualityOperator(int lineNumber, Block parentBlock)
+	{
+		super(lineNumber, parentBlock);
+		returnsBoolean = true;
+	}
 
-    /**
-     *
-     * @param t
-     * @return
-     */
-    public int generateBF()
-    {
-        if (!((!left.returnsBoolean) && (!right.returnsBoolean)))
-        {
-            System.err.println("= must take two non boolean operands");
-            System.exit(-1);
-        }
-        int x = left.generateBF(), y = right.generateBF();
-        int res = statics.t.equal(x, y);
-        statics.t.clear(y);
-        statics.t.free(y);
-        return res;
-    }
+	/**
+	 *
+	 * @param t
+	 * @return
+	 */
+	public int generateBF()
+	{
+		int x = 0, y = 0;
+		boolean rightIsConstant = false;
+		boolean leftIsConstant = false;
+		if ((left instanceof ConstantNode) && (right instanceof ConstantNode))
+		{
+			rightIsConstant = true;
+		} else if (right instanceof ConstantNode)
+		{
+			rightIsConstant = true;
+		} else if (left instanceof ConstantNode)
+		{
+			leftIsConstant = true;
+		}
+
+		if (!leftIsConstant && !rightIsConstant)
+		{
+			boolean yIsVariable = false;
+			if (right instanceof VariableNode)
+			{
+				x = left.generateBF();
+				y = ((VariableNode) right).getVariable().getMemoryPosition();
+				yIsVariable = true;
+			} else if (left instanceof VariableNode)
+			{
+				x = right.generateBF();
+				y = ((VariableNode) left).getVariable().getMemoryPosition();
+				yIsVariable = true;
+			}
+			int temp = statics.t.alloc();
+			statics.t.loop(y);
+			statics.t.dec(x);
+			if (yIsVariable)
+			{
+				statics.t.inc(temp);
+			}
+			statics.t.deloop(y);
+			if (yIsVariable)
+			{
+				statics.t.move(y, temp);
+			}
+			statics.t.inc(temp);
+			statics.t.loop(x);
+			statics.t.dec(temp);
+			statics.t.celoop(x);
+			statics.t.free(x);
+			if (!yIsVariable)
+			{
+				statics.t.free(y);
+			}
+			return temp;
+		} else if (leftIsConstant)
+		{
+			x = right.generateBF();
+			y = statics.t.alloc();
+			statics.t.minus(x, ((ConstantNode) left).getValue());
+			statics.t.inc(y);
+			statics.t.loop(x);
+			statics.t.dec(y);
+			statics.t.celoop(x);
+			statics.t.clear(x);
+			statics.t.free(x);
+			return y;
+		} else
+		{
+			x = left.generateBF();
+			y = statics.t.alloc();
+			statics.t.minus(x, ((ConstantNode) right).getValue());
+			statics.t.inc(y);
+			statics.t.loop(x);
+			statics.t.dec(y);
+			statics.t.celoop(x);
+			statics.t.clear(x);
+			statics.t.free(x);
+			return y;
+		}
+	}
 }
--- brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/nodetypes/ConstantNode.java
+++ brainfuck-compiler/src/brainfuckcompiler/compiler/expressions/nodetypes/ConstantNode.java
@@ -11,37 +11,42 @@ import brainfuckcompiler.statics;
 public class ConstantNode extends Node
 {
 
-    int value = 0;
+	private int value = 0;
 
-    public ConstantNode(int lineNumber, Block parentBlock)
-    {
-        super(lineNumber, parentBlock);
-    }
+	public ConstantNode(int lineNumber, Block parentBlock)
+	{
+		super(lineNumber, parentBlock);
+	}
 
-    /**
-     *
-     * @param tokens
-     * @param index
-     * @return
-     */
-    public int populate(String[] tokens, int index)
-    {
-        value = Integer.parseInt(tokens[index + 1]);
-        return index;
-    }
+	/**
+	 *
+	 * @param tokens
+	 * @param index
+	 * @return
+	 */
+	public int populate(String[] tokens, int index)
+	{
+		value = Integer.parseInt(tokens[index + 1]);
+		return index;
+	}
 
-    /**
-     *
-     * @return
-     */
-    public int generateBF()
-    {
-        int address = statics.t.alloc();
-        if (value != 0)
-        {
-            statics.t.to(address);
-            statics.t.plus(value);
-        }
-        return address;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	public int generateBF()
+	{
+		int address = statics.t.alloc();
+		if (value != 0)
+		{
+			statics.t.to(address);
+			statics.t.plus(value);
+		}
+		return address;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
 }
